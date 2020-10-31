182dd0ed74e983396989532064736987b5409002
Frodo <frodo@bagend.com>
2020-10-31 10:42:38 +0100

    first commit

---
+++ src/test/java/no/frodo/dockerdemo/DockerDemoTests.java
@@ -0,0 +1,13 @@
+package no.frodo.dockerdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerDemoTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
