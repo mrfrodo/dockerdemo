96a60027077b97d934fa0837c6aa77dc151f85a1
Frodo <frodo@bagend.com>
2020-11-13 07:47:34 +0100

    Evict cache every 10 seconds

--- src/main/java/no/frodo/dd/DDApplication.java
+++ src/main/java/no/frodo/dd/DDApplication.java
@@ -2,8 +2,10 @@ package no.frodo.dd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class DDApplication {
 
 	public static void main(String[] args) {
--- src/main/java/no/frodo/dd/config/CacheConfig.java
+++ src/main/java/no/frodo/dd/config/CacheConfig.java
@@ -1,9 +1,11 @@
 package no.frodo.dd.config;
 
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.Scheduled;
 
 @Configuration
 @EnableCaching
@@ -12,6 +14,13 @@ public class CacheConfig {
     @Bean
     public ConcurrentMapCacheManager cacheManager() {
         return new ConcurrentMapCacheManager("getCustomer2");
+    }
+
+    //execute once every 10 second (fixedDelay=10000)
+    @CacheEvict(allEntries = true, cacheNames = { "getCustomer2" })
+    @Scheduled(fixedDelay = 10000)
+    public void cacheEvict() {
+        System.out.println("** evict cache");
     }
 
 }
