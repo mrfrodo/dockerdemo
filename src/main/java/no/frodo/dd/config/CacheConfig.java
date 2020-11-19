9488be4f65af7573cc724c9c951a0b7f58da9ddf
Frodo <frodo@bagend.com>
2020-11-19 06:52:34 +0100

    Moved scheduling annotation

--- src/main/java/no/frodo/dd/config/CacheConfig.java
+++ src/main/java/no/frodo/dd/config/CacheConfig.java
@@ -5,10 +5,12 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
 @Configuration
 @EnableCaching
+@EnableScheduling
 public class CacheConfig {
 
     @Bean
