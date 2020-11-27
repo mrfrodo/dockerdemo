1ad451e1482e31076ba9a22a5f294079f2919e97
Frodo <frodo@bagend.com>
2020-11-27 07:52:15 +0100

    Added jwt security

---
+++ src/main/java/no/frodo/dd/filter/JwtRequestFilter.java
@@ -0,0 +1,40 @@
+package no.frodo.dd.filter;
+
+import no.frodo.dd.controller.CustomerController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class JwtRequestFilter extends OncePerRequestFilter {
+
+    private static final Logger logger = LoggerFactory.getLogger(CustomerController.class);
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+            throws ServletException, IOException {
+
+        final String authorizationHeader = request.getHeader("Authorization");
+        logger.info("Authorization header '{}'", authorizationHeader);
+        String jwt = null;
+        String username = null;
+
+        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+            jwt = authorizationHeader.substring(7);
+            username = jwtUtil.extractUsername(jwt);
+        }
+
+        chain.doFilter(request, response);
+    }
+}
